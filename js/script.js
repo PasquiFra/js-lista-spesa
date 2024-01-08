b884d82e37b79b653f5b87e1542ef10781a3c894
Francesco <francesco.pasquinoni@hotmail.it>
2024-01-08 16:23:44 +0100

    impostata lista della spesa e funzione per inserire gli articoli in lista

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 console.log ("JS OK");
 
+// imposto il dom degli elementi
+
+const groceryList = document.getElementById("grocery-list");
+
+const products = ["dentifricio", "latte", "biscotti", "acqua", "uova", "affettati", "pasta", "zucchero", "sale", "pepe", "frutta", "verdura"];
+
+console.table(products);
+
+let i = 0; 
+
+while (i < products.length) {
+    
+    groceryList.innerHTML += `<li class="list-group-item">${products [i]}</li>`;
+    console.log(groceryList, "i:", i);
+
+}
